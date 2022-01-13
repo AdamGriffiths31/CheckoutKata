b28a69112283616de717e97e025619bd9e823284
AdamGriffiths31 <60197620+AdamGriffiths31@users.noreply.github.com>
2022-01-13 14:53:56 +0000

    Add project files.

---
+++ Models/CheckoutItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutKata.Models
+{
+    class CheckoutItem
+    {
+        public char SKU { get; set; } //Char works for the current examples but will cause issues after z.
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public CheckoutItem(char productSKU, int ProductQuantity)
+        {
+            SKU = productSKU;
+            Quantity = ProductQuantity;
+            switch (SKU)
+            {
+                case 'A':
+                    UnitPrice = 10;
+                    break;
+                case 'B':
+                    UnitPrice = 15;
+                    break;
+                case 'C':
+                    UnitPrice = 40;
+                    break;
+                case 'D':
+                    UnitPrice = 55;
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using CheckoutKata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckoutKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            P.CreateBasket();
+        }
+        public void CreateBasket()
+        {
+            List<CheckoutItem> basket = new List<CheckoutItem>();
+            basket = UpdateBasket(basket, 'D', 1);
+            DisplayBasket(basket);
+            Console.ReadLine();
+        }
+        public void DisplayBasket(List<CheckoutItem> Basket)
+        {
+            if (Basket == null)
+            {
+                Console.WriteLine("Your basket is null");
+            }
+            if (Basket.Count == 0)
+            {
+                Console.WriteLine("Your basket is empty");
+            }
+            foreach(var item in Basket)
+            {
+                Console.WriteLine($"{item.SKU} (£{item.UnitPrice})");
+            }
+        }
+        public List<CheckoutItem> UpdateBasket(List<CheckoutItem> Basket,char SKU,int Quantity)
+        {
+
+            if (Basket.Count == 0)
+            {
+                CheckoutItem item = new CheckoutItem(SKU, Quantity);
+                Basket.Add(item);
+            }
+
+            return Basket;
+        }
+
+    }
+}
